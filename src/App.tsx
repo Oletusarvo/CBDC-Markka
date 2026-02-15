2aa19e8315031d041876b94b40fa928113ab5237
unknown <jens.osterberg@gmail.com>
2026-02-15 20:50:43 +0200

    Added manifest and service worker

--- src/App.tsx
+++ src/App.tsx
@@ -12,59 +12,63 @@ import { QueryProvider } from './providers/query-provider';
 import { Spinner } from './components/spinner';
 import { LogoutScreen } from './screen/logout-screen';
 import { TransactionScreen } from './screen/transaction-screen';
+import { ServiceWorkerLoader } from './components/service-worker-loader';
 
 export function App() {
   return (
-    <QueryProvider>
-      <HashRouter>
-        <Suspense fallback={<Spinner />}>
-          <AuthProvider>
-            <Routes>
-              <Route
-                path='/'
-                element={<MainLayout />}>
+    <>
+      <QueryProvider>
+        <HashRouter>
+          <Suspense fallback={<Spinner />}>
+            <AuthProvider>
+              <Routes>
                 <Route
-                  path='login'
-                  element={<LoginScreen />}
-                />
-                <Route
-                  path='register'
-                  element={<RegisterScreen />}
-                />
-              </Route>
-
-              <Route
-                path='/auth'
-                element={<AuthLayout />}>
-                <Route
-                  path='overview'
-                  element={<OverviewLayout />}>
+                  path='/'
+                  element={<MainLayout />}>
                   <Route
-                    path='send'
-                    element={<SendScreen />}
+                    path='login'
+                    element={<LoginScreen />}
                   />
                   <Route
-                    path='receive'
-                    element={<ReceiveScreen />}
-                  />
-                  <Route
-                    path='transaction/:id'
-                    element={<TransactionScreen />}
+                    path='register'
+                    element={<RegisterScreen />}
                   />
                 </Route>
+
                 <Route
-                  path='logout'
-                  element={<LogoutScreen />}
-                />
-                <Route
-                  path='transactions'
-                  element={null}
-                />
-              </Route>
-            </Routes>
-          </AuthProvider>
-        </Suspense>
-      </HashRouter>
-    </QueryProvider>
+                  path='/auth'
+                  element={<AuthLayout />}>
+                  <Route
+                    path='overview'
+                    element={<OverviewLayout />}>
+                    <Route
+                      path='send'
+                      element={<SendScreen />}
+                    />
+                    <Route
+                      path='receive'
+                      element={<ReceiveScreen />}
+                    />
+                    <Route
+                      path='transaction/:id'
+                      element={<TransactionScreen />}
+                    />
+                  </Route>
+                  <Route
+                    path='logout'
+                    element={<LogoutScreen />}
+                  />
+                  <Route
+                    path='transactions'
+                    element={null}
+                  />
+                </Route>
+              </Routes>
+            </AuthProvider>
+          </Suspense>
+        </HashRouter>
+      </QueryProvider>
+      <ServiceWorkerLoader />
+    </>
   );
 }
