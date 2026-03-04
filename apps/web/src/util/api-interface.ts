d60fde66c1395c03e3068971dfe0ff57baa80d86
unknown <jens.osterberg@gmail.com>
2026-03-04 23:13:03 +0200

    2.0.0 commit 3

--- apps/web/src/util/api-interface.ts
+++ apps/web/src/util/api-interface.ts
@@ -1,5 +1,7 @@
 import { ApiInterface } from '@cbdc-markka/utils-api';
 
 export const apiInterface = new ApiInterface(
-  import.meta.env.PROD ? 'https://cbdc-markka.onrender.com/api' : 'http://localhost:3000/api',
+  import.meta.env.PROD
+    ? 'https://cbdc-markka-server.onrender.com/api'
+    : 'http://localhost:3000/api',
 );
