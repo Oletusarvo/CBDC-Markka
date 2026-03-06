0ca921b197e8a259ec2caafcfae9f15e391c5f8d
unknown <jens.osterberg@gmail.com>
2026-03-06 23:23:28 +0200

    2.0.0 commit 25

--- apps/server/src/features/auth/handlers/logout-handler.ts
+++ apps/server/src/features/auth/handlers/logout-handler.ts
@@ -2,6 +2,11 @@ import { serverConfig } from '../../../../server-config';
 import { createHandler } from '../../../utils/create-handler';
 
 export const logoutHandler = createHandler(async (req, res) => {
-  res.clearCookie(serverConfig.accessTokenName);
+  res.clearCookie(serverConfig.accessTokenName, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+  });
   return res.status(200).end();
 });
